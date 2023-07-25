84fb064a8cb4c04b8a6b2e7820a580d0d73f14d5
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-07-25 23:10:01 +0530

    main

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -57,121 +57,121 @@ const Calculator = () => {
         />
         <div className="grid grid-cols-4 gap-2 mt-4">
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => clear()}
           >
             AC
           </button>
           <button
-            className="bg-white py-7 px-5  lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md "
+            className="bg-white py-7 px-5  lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md "
             onClick={() => deleteValue()}
           >
             DEL
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md "
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md "
             onClick={() => showHideHistory()}
           >
             HIS
           </button>
           <button
-            className="bg-white py-7 px-5  lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md "
+            className="bg-white py-7 px-5  lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md "
             onClick={() => deleteValue()}
           >
             CL
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("7")}
           >
             7
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("8")}
           >
             8
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("9")}
           >
             9
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("/")}
           >
             /
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("4")}
           >
             4
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("5")}
           >
             5
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("6")}
           >
             6
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("*")}
           >
             *
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("1")}
           >
             1
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("2")}
           >
             2
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("3")}
           >
             3
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("-")}
           >
             -
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("0")}
           >
             0
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => handleButtonClick("+")}
           >
             +
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md "
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md "
             onClick={() => handleButtonClick("+")}
           >
             .
           </button>
           <button
-            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm hover:bg-opacity-20 hover:shadow-md"
+            className="bg-white py-7 px-5 lg:py-7 rounded-md bg-opacity-10 backdrop-filter backdrop-blur-sm lg:hover:bg-opacity-20 lg:hover:shadow-md"
             onClick={() => calculate()}
           >
             =
